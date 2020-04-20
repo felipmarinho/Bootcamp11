da19987da265b17330b03dc7196f69eca4fb6270
Felipe Marinho <felipmarinho@gmail.com>
2020-04-20 20:37:31 -0300

    Adding new project

--- Level_01/mobile/src/index.js
+++ Level_01/mobile/src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 
 import api from './services/api';
 
@@ -12,6 +12,16 @@ export default function App() {
 		})
 	}, []);
 
+	async function handleAddProject() {
+		const response = await api.post('projects', {
+			title: `Novo projeto ${Date.now()}`,
+			owner: 'Felipe Marinho'
+		})
+
+		const project = response.data;
+		setProjects([...projects, project])
+	}
+
 	return (
 		<>
 			<StatusBar barStyle='light-content' backgroundColor='#7159c1' />
@@ -21,8 +31,15 @@ export default function App() {
 					keyExtractor={project => project.id}
 					renderItem={({ item: project }) => (
 						<Text style={styles.project}>{project.title}</Text>
-						)}
-					/>
+					)}
+				/>
+				<TouchableOpacity
+					activeOpacity={0.6}
+					style={styles.button}
+					onPress={handleAddProject}
+				>
+					<Text style={styles.buttonText}>Adiconar projeto</Text>
+				</TouchableOpacity>
 			</SafeAreaView>
 		</>
 	);
@@ -36,5 +53,17 @@ const styles = StyleSheet.create({
 	project: {
 		color: '#FFF',
 		fontSize: 20,
+	},
+	button: {
+		backgroundColor: '#FFF',
+		margin: 20,
+		height: 50,
+		borderRadius: 4,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	buttonText: {
+		fontWeight: 'bold',
+		fontSize: 16,
 	}
 })
