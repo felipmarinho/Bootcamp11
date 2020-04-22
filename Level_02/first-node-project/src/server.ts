09fb91aa85be9bfae78e23c9aa03c0bbab414bfd
Felipe Marinho <felipmarinho@gmail.com>
2020-04-22 19:09:03 -0300

    ESLint

--- Level_02/first-node-project/src/server.ts
+++ Level_02/first-node-project/src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-	return response.json({ message: 'Hello world' });
-})
+app.get('/', (request, response) => response.json({ message: 'Hello world' }));
 
 app.listen(3333, () => {
-	console.log('🤘 Server started on 🚪 3333!');
-})
+  console.log('🤘 Server started on 🚪 3333!');
+});
