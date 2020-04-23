867a04bb5aab7d539d38ad62613b8164b94719ab
Felipe Marinho <felipmarinho@gmail.com>
2020-04-23 06:36:08 -0300

    Debugging NodeJS

--- Level_02/first-node-project/src/routes/index.ts
+++ Level_02/first-node-project/src/routes/index.ts
@@ -2,8 +2,12 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello world' }),
-);
+routes.post('/user', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = { name, email };
+
+  return response.json(user);
+});
 
 export default routes;
