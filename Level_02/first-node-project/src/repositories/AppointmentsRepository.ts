882ac9d40a0ef07a7f9d09a993dc67d100fbcc26
Felipe Marinho <felipmarinho@gmail.com>
2020-04-25 09:24:28 -0300

    Creating repository

---
+++ Level_02/first-node-project/src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,26 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+class AppointmentsRepository {
+  constructor() {
+    this.appointments = [];
+  }
+
+  private appointments: Appointment[];
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return findAppointment || null;
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+    this.appointments.push(appointment);
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
