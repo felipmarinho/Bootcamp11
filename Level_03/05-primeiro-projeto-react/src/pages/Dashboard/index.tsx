a3b57ead985f6cc4342de98603b7645248d29e2f
Felipe Marinho <felipmarinho@gmail.com>
2020-05-11 05:02:06 -0300

    Creating Routes

---
+++ Level_03/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// Utilizando TS é necessário informar o tipo de um componente e para isso
+// utilizamos : React.FC -> React.FunctionalComponent
+const Dashboard: React.FC = () => {
+  return <h1>Dashboard</h1>;
+};
+
+export default Dashboard;
